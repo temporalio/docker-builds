527db58331aa95dc0f6816517e5502b95127192c
Rob Holland <rob.holland@gmail.com>
2024-06-17 17:36:06 +0100

    Default TEMPORAL_BROADCAST_ADDRESS to primary IP if BIND_ON_IP is 0.0.0.0. (#216)
    We require broadcast address to be set if listening on all interfaces, but it
    is not possible for users to know which IP they will have via environment
    variables on all platforms.
    
    Set a sensible default for broadcast address to reduce the need for custom images.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,11 @@ set -eu -o pipefail
 : "${BIND_ON_IP:=$(getent hosts "$(hostname)" | awk '{print $1;}')}"
 export BIND_ON_IP
 
+if [[ "${BIND_ON_IP}" == "0.0.0.0" ]]; then
+    : "${TEMPORAL_BROADCAST_ADDRESS:=$(getent hosts "$(hostname)" | awk '{print $1;}')}"
+    export TEMPORAL_BROADCAST_ADDRESS
+fi
+
 # check TEMPORAL_ADDRESS is not empty
 if [[ -z "${TEMPORAL_ADDRESS:-}" ]]; then
     echo "TEMPORAL_ADDRESS is not set, setting it to ${BIND_ON_IP}:7233"
